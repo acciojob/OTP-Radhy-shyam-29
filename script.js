71fdf072935e259fc97d635744179e891e4dcd8b
Radhy Shyam <radhy2929@gmail.com>
2026-02-18 04:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,29 @@ const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
 
-  input.addEventListener("keyup", function (e) {
+  input.addEventListener("input", function () {
 
-    // Allow only digits
-    if (this.value && !(this.value >= "0" && this.value <= "9")) {
+    // allow only digits
+    if (!(this.value >= "0" && this.value <= "9")) {
       this.value = "";
       return;
     }
 
-    // Forward move
-    if (e.key !== "Backspace" && this.value !== "") {
-      if (index < inputs.length - 1) {
-        inputs[index + 1].focus();
-      }
+    // move forward immediately
+    if (this.value !== "" && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
+  });
 
-    // Backspace logic
+  input.addEventListener("keydown", function (e) {
+
     if (e.key === "Backspace") {
 
-      if (this.value === "" && index > 0) {
-        inputs[index - 1].value = "";
+      // move to previous immediately
+      if (index > 0) {
         inputs[index - 1].focus();
       }
     }
-
   });
 
 });
